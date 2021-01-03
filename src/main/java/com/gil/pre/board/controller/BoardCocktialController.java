6e1f3d322d4000926a9f8ca7acdf18a24c641ab5
Yugilsang <ygs3334@gmail.com>
2021-01-03 19:30:31 +0900

    add 'awf'

--- src/main/java/com/gil/pre/board/controller/BoardCocktialController.java
+++ src/main/java/com/gil/pre/board/controller/BoardCocktialController.java
@@ -29,7 +29,6 @@ public class BoardCocktialController {
 		ServletContext context =request.getSession().getServletContext();
 		String RealPath = context.getRealPath(save);
 		MultipartFile uploadfile = vo.getUploadFile();
-
 		
 		long time = System.currentTimeMillis();
 		SimpleDateFormat daytime = new SimpleDateFormat("HH-mm-ss");
@@ -77,7 +76,7 @@ public class BoardCocktialController {
 	//칵테일 글 조회하기
 	@RequestMapping(value="/cocktail_content.do")
 	public String selectNoticeContent(BoardVO vo, Model model) {
-		model.addAttribute("nb", s.selectOneBoard(vo));
+		model.addAttribute("c", s.selectOneBoard(vo));
 		return "cocktail_content.jsp";
 	}
 	
@@ -85,7 +84,7 @@ public class BoardCocktialController {
 	@RequestMapping(value="/modifyCocktail.do")
 	public String modifyNotice(BoardVO vo, Model model) {
 		model.addAttribute("c", s.selectOneBoard(vo));
-		return "notice_modify.jsp";
+		return "cocktail_modify.jsp";
 	}
 	
 	//칵테일 수정하기
